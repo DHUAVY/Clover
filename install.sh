3af20e642676078a4ea47b98cad66c34444e8b8d
DHUAVY <3484432441@qq.com>
2023-10-17 13:44:45 +0000

    chore:add pip3 install cython==0.29.36

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ pip3 install einops timm prettytable nvidia-ml-py3 Cython numpy matplotlib Pillo
 pip3 install -U fire lmdb tqdm sklearn pandas
 pip3 install --upgrade --force-reinstall opencv-python-headless
 pip3 install -U opencv-python-headless
-
+pip3 install cython==0.29.36
 
 pip3 install mmcv-full==1.3.18 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html
 
@@ -29,4 +29,4 @@ pip3 install nltk
 pip3 install ffmpeg-python
 
 pip3 install SceneGraphParser
-python3 -m spacy download en
+python3 -m spacy download en_core_web_sm
