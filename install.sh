a010b1e912c28d6e9ca9ec4c297bab0cbdfbd3ec
DHUAVY <3484432441@qq.com>
2023-11-04 12:57:03 +0000

    chore: change cython to Cython

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ pip3 uninstall mmaction2 -y
 pip3 uninstall mmcv-full -y
 
 # conda install av -c conda-forge -y
+pip3 install Cython==0.29.36    
 pip3 install av
 pip3 install PyTurboJPEG moviepy decord==0.4.1
 pip3 install einops timm prettytable nvidia-ml-py3 Cython numpy matplotlib Pillow six terminaltables IPython mmpycocotools albumentations>=0.3.2
 pip3 install -U fire lmdb tqdm sklearn pandas
 pip3 install --upgrade --force-reinstall opencv-python-headless
 pip3 install -U opencv-python-headless
-pip3 install cython==0.29.36
 
 pip3 install mmcv-full==1.3.18 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html
 
