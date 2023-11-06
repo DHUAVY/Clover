9138decdf809a20d27749dbcc9fb761fbe615565
DHUAVY <3484432441@qq.com>
2023-11-06 16:30:04 +0000

    chore:change config

--- ddist_train_multiloader.sh
+++ ddist_train_multiloader.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES="0,1,3,4,5,6"
 export OMP_NUM_THREADS=6
 
-CONFIG='/data_sas/fhr/Clover/configs/exp_local/pretrain_msrvtt_cc3m.py'
+CONFIG='/data_sas/fhr/Clover/configs/exp_local/pretrain_msrvtt_cc3m_CKA_gated.py'
 GPUS=6
 HOSTS=127.0.0.1
 PORT=10001
